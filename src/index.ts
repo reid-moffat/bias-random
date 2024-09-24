63e85eb650b7a79a85779d7f8c1279055ab9f15f
Reid Moffat <18rem8@queensu.ca>
2024-09-24 11:12:55 -0400

    improved documentation + code quality

--- src/index.ts
+++ src/index.ts
@@ -6,31 +6,32 @@ interface BiasedRandomOptions {
 }
 
 /**
- * Generates a random number, biased towards lower of higher numbers
+ * Generates a random number, biased towards lower or higher numbers with the level of your choice
+ *
+ * @param upperBias Set to true to bias this towards higher numbers instead of lower numbers. E.g. with a range of 0
+ * to 1, a biasLevel of 2, the average value would be 2/3 (1 - (1 / 3))
+ * @param biasLevel How strongly the bias is, default 2. The resulting values have an average value of 1 / (biasLevel
+ * + 1) (given upperBias = false and a range of 0 to 1). Can be a decimal number, but must be at least 1 (for less than
+ * 1, this is equivalent to using an upperBias, e.g. a bias of 0.5 is the same as an upper bias of 2)
+ * @param min Minimum value of the result, default 0. This scales the result, but keeps the relative bias
+ * @param max Maximum value of the result, default 1. This scales the result, but keeps the relative bias
  */
-const biasedRandom = ({
-  upperBias = false,
-  biasLevel = 2,
-  min = 0,
-  max = 1
-}: BiasedRandomOptions = {}): number => {
-  if (biasLevel < 2) {
-    throw new Error('biasLevel must be 2 or greater.');
+const biasedRandom = ({ upperBias = false, biasLevel = 2, min = 0, max = 1 }: BiasedRandomOptions = {}): number => {
+
+  if (biasLevel < 1) {
+    throw new TypeError(`Parameter 'biasLevel' must be at least 1 (value: ${biasLevel}); use upperBias to swap bias direction`);
+  }
+  if (min >= max) {
+    throw new TypeError(`Parameter 'min' muist be less than 'max' (you can flip them for a valid result). Min value: ${min} Max value: ${max}`);
   }
 
-  const range = max - min;
-  let randomValue = Math.random();
+  let randomValue = Math.pow(Math.random(), biasLevel);
 
-  // Apply bias transformation
-  randomValue = Math.pow(randomValue, biasLevel);
-
-  // Reverse for upper bias
   if (upperBias) {
     randomValue = 1 - randomValue;
   }
 
-  // Scale to range
-  return min + randomValue * range;
+  return min + randomValue * (max - min);
 }
 
 export default biasedRandom;
