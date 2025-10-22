3c694380197abe9707c312f5a5b57c870d273623
Reid Moffat <18rem8@queensu.ca>
2025-10-22 11:43:44 -0400

    Properly document each option field

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,14 @@
  * The whole object as well as every field is optional.
  */
 type BiasedRandomOptions = {
-  upperBias?: boolean; // Bias towards the higher number if true, otherwise lower
-  biasLevel?: number;  // Bias factor, must be 1 or greater, default is 2 (1 is no bias)
-  min?: number;        // Minimum value, default is 0
-  max?: number;        // Maximum value, default is 1
+  /** Bias towards the higher number if true, otherwise lower */
+  upperBias?: boolean;
+  /** Bias factor, must be 1 or greater, default is 2 (1 is no bias) */
+  biasLevel?: number;
+  /** Minimum value, default is 0 */
+  min?: number;
+  /** Maximum value, default is 1 */
+  max?: number;
 }
 
 /**
