9e33d7e453927af73c61cb64195a32a3a3f09e77
Reid Moffat <18rem8@queensu.ca>
2026-01-12 10:54:36 -0500

    Remove redundant default documentation

--- src/BiasRandom.ts
+++ src/BiasRandom.ts
@@ -1,19 +1,19 @@
 import BiasedRandomOptions from "./options.ts";
 
 /**
- * Generates a random number between `min` and `max`, with an optional bias towards the lower or upper bound.
+ * Generates a random number between `min` and `max`, with an optional bias towards the lower or upper bound
  *
- * @param {Object} [options] - Optional configuration object to adjust the behavior of the random number generation.
- * @param {boolean} [options.upperBias=false] - If true, biases the result towards the higher bound (`max`), otherwise biases towards the lower bound (`min`). Default is `false`.
- * @param {number} [options.biasLevel=2] - Determines the strength of the bias (power to raise the random result by). Must be 1 or greater, where `1` means no bias and higher values increase the bias. Default is `2`.
- * @param {number} [options.min=0] - The minimum value for the random number range. Default is `0`.
- * @param {number} [options.max=1] - The maximum value for the random number range. Default is `1`.
+ * @param {Object} [options] - Optional configuration object to adjust the behavior of the random number generation
+ * @param {boolean} [options.upperBias=false] - If true, biases the result towards the higher bound (`max`), otherwise biases towards the lower bound (`min`)
+ * @param {number} [options.biasLevel=2] - Determines the strength of the bias (power to raise the random result by). Must be 1 or greater, where `1` means no bias and higher values increase the bias
+ * @param {number} [options.min=0] - The minimum value for the random number range
+ * @param {number} [options.max=1] - The maximum value for the random number range
  *
- * @returns {number} A random number between `min` and `max`, optionally biased based on the provided options.
+ * @returns {number} A random number between `min` and `max`, optionally biased based on the provided options
  *
- * @throws {TypeError} If `biasLevel` is not a number or is less than 1.
- * @throws {TypeError} If `min` is not less than `max`, or if either `min` or `max` is not a number.
- * @throws {TypeError} If `upperBias` is not a boolean.
+ * @throws {TypeError} If `biasLevel` is not a number or is less than 1
+ * @throws {TypeError} If `min` is not less than `max`, or if either `min` or `max` is not a number
+ * @throws {TypeError} If `upperBias` is not a boolean
  */
 const biasedRandom: ((opts?: BiasedRandomOptions) => number) = ({upperBias = false, biasLevel = 2, min = 0, max = 1}: BiasedRandomOptions = {}): number => {
 
