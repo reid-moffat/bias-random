177d04b725db4e7dc775759d24266620a33f32c1
Reid Moffat <18rem8@queensu.ca>
2026-02-05 14:19:01 -0500

    Format + all checks + pnpm refs

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,10 +6,7 @@ import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
     {
-        ignores: [
-            'dist/',
-            'node_modules/'
-        ]
+        ignores: ['dist/', 'node_modules/'],
     },
     {
         files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
